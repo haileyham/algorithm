0096391d06520154022c14f4e272f51cacff0204
hailey ham <roserealslow@gmail.com>
2024-06-24 19:29:16 +0900

    문제 해결 패턴 - 분활과 정복

---
+++ 문제해결패턴/분할과 정복 패턴/이진탐색.js
@@ -0,0 +1,20 @@
+function search(array, val) {
+  let min = 0;
+  let max = array.length - 1;
+
+  while (min <= max) {
+    let middle = Math.floor((min + max) / 2);
+    let currentElement = array[middle];
+
+    if (array[middle] < val) {
+      min = middle + 1;
+    } else if (array[middle] > val) {
+      max = middle - 1;
+    } else {
+      return middle;
+    }
+  }
+  return -1;
+}
+
+[1, 2, 3, 5, 6, 8, 9, 12, 15, 16, 29]
